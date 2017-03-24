fed15ba1177873f4bbf30c2bb97818970bc2cb15
Santiago Orobon <sorobon@idealista.com>
2017-03-24 14:39:23 +0100

    #19 Prometheus metrics customization - More tests

--- tests/test_ansible.py
+++ tests/test_ansible.py
@@ -12,6 +12,11 @@ def AnsibleVars(Ansible):
     return Ansible("include_vars", "tests/group_vars/group01.yml")["ansible_facts"]
 
 
+@pytest.fixture()
+def Hostname(TestinfraBackend):
+    return TestinfraBackend.get_hostname()
+
+
 def test_nginx_user(User, Group, AnsibleDefaults):
     assert User(AnsibleDefaults["nginx_user"]).exists
     assert Group(AnsibleDefaults["nginx_group"]).exists
@@ -62,13 +67,33 @@ def test_nginx_service(File, Service, Socket, AnsibleVars):
     assert Socket("tcp://0.0.0.0:{0}".format(web2_port)).is_listening
 
 
-def test_nginx_prometheus_service(File, Service, Socket, AnsibleVars):
-    metrics_port = AnsibleVars["nginx_prometheus_metrics_port"]
-    metrics_path = AnsibleVars["nginx_prometheus_metrics_path"]
-    hostname = AnsibleVars["hostname"]
-    assert File("/lib/systemd/system/nginx.service").exists
-    assert Service("nginx").is_enabled
-    assert Service("nginx").is_running
-    assert Socket("tcp://0.0.0.0:9999").is_listening
-    assert Socket("tcp://0.0.0.0:{0}".format(metrics_port)).is_listening
-    assert 200 == urllib2.urlopen('http://{0}:{1}{2}'.format(hostname, metrics_port, metrics_path)).getcode()
+def test_nginx_prometheus_service(File, Service, Socket, AnsibleVars, Hostname):
+    if Hostname in ("nginx"):
+        metrics_port = AnsibleVars["nginx_prometheus_metrics_port"]
+        metrics_path = AnsibleVars["nginx_prometheus_metrics_path"]
+        assert File("/lib/systemd/system/nginx.service").exists
+        assert Service("nginx").is_enabled
+        assert Service("nginx").is_running
+        assert Socket("tcp://0.0.0.0:9999").is_listening
+        assert File("/etc/nginx/sites-available/prometheus").exists
+        assert File("/etc/nginx/sites-enabled/prometheus").exists
+        assert File("/etc/nginx/extra-conf/prometheus.conf").exists
+        assert Socket("tcp://0.0.0.0:{0}".format(metrics_port)).is_listening
+        assert 200 == urllib2.urlopen('http://{0}:{1}/{2}'.format(Hostname, metrics_port, metrics_path)).getcode()
+
+
+def test_nginx_not_has_prometheus_service(File, Service, Socket, AnsibleVars, Hostname):
+    if Hostname == "nginx2":
+        metrics_port = AnsibleVars["nginx_prometheus_metrics_port"]
+        assert File("/lib/systemd/system/nginx.service").exists
+        assert Service("nginx").is_enabled
+        assert Service("nginx").is_running
+        assert Socket("tcp://0.0.0.0:9999").is_listening
+        prometheusSiteFile = File("/etc/nginx/sites-available/prometheus")
+        assert not prometheusSiteFile.exists
+        prometheusSiteFile = File("/etc/nginx/sites-enabled/prometheus")
+        assert not prometheusSiteFile.exists
+        prometheusConfFile = File("/etc/nginx/extra-conf/prometheus.conf")
+        assert not prometheusConfFile.exists
+        metricSocket = Socket("tcp://0.0.0.0:{0}".format(metrics_port))
+        assert not metricSocket.is_listening
